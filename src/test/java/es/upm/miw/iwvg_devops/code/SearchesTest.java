aabd13598cfea08c08d2333ee89a3401c943aace
JorgeDSDP <jorge.dasilva@alumni.mondragon.edu>
2023-09-21 12:32:31 +0200

    Created UsersDatabase #6

---
+++ src/test/java/es/upm/miw/iwvg_devops/code/SearchesTest.java
@@ -0,0 +1,15 @@
+package es.upm.miw.iwvg_devops.code;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SearchesTest {
+    @Test
+    void testUserDatabase() {
+        assertEquals(6, new UsersDatabase().findAll().toList().size());
+    }
+}
