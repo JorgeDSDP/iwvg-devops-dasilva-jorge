8cab669052f5d4ce496e999054792f7d38711e6e
JorgeDSDP <jorge.dasilva@alumni.mondragon.edu>
2023-09-21 15:42:18 +0200

    Enhanced FractionTest #7

--- src/test/java/es/upm/miw/iwvg_devops/code/SearchesTest.java
+++ src/test/java/es/upm/miw/iwvg_devops/code/SearchesTest.java
@@ -2,9 +2,6 @@ package es.upm.miw.iwvg_devops.code;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SearchesTest {
